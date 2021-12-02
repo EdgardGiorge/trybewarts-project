fa7815717f9adf831831a049bf5ad32b27676a58
Eduardo Souza <superedu2012@gmail.com>
2021-12-02 15:53:59 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const email = document.getElementById('loginEmail');
+const password = document.getElementById('loginPassword');
+const loginButton = document.getElementById('loginButton');
+
+function login() {  
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+
+loginButton.addEventListener('click', login);
