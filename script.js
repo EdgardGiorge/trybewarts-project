7b18ebf23d06ec134d0c98c919c577f2dd454e76
Eduardo Souza <superedu2012@gmail.com>
2021-12-02 17:25:17 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const email = document.getElementById('loginEmail');
 const password = document.getElementById('loginPassword');
 const loginButton = document.getElementById('loginButton');
+const submitButton = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
+
+submitButton.disabled = true;
 
 function login() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -10,4 +14,13 @@ function login() {
   }
 }
 
+function checkInfo() {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
+
+agreement.addEventListener('click', checkInfo);
 loginButton.addEventListener('click', login);
